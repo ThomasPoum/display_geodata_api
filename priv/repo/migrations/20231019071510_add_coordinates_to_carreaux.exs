acdae386b88d84a74d07c45018886de255491177
ThomasPoum <thomas.poumarede@gmail.com>
2023-10-19 13:06:27 +0200

    modifs migration to accept creation extension within migration -for fly.io-

--- priv/repo/migrations/20231019071510_add_coordinates_to_carreaux.exs
+++ priv/repo/migrations/20231019071510_add_coordinates_to_carreaux.exs
@@ -1,9 +1,18 @@
 defmodule DisplayGeodataApi.Repo.Migrations.AddCoordinatesToCarreaux do
   use Ecto.Migration
 
-  def change do
+  def up do
+    execute "CREATE EXTENSION IF NOT EXISTS postgis;"
     alter table(:carreaux) do
       add :coordinates, :geometry, srid: 4326  # Le SRID est optionnel
+    end
+  end
+
+  def down do
+    # execute "DROP EXTENSION IF EXISTS postgis;"
+
+    alter table(:carreaux) do
+      remove :coordinates
     end
   end
 end
